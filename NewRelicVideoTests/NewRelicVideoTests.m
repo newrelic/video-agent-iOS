a6cce95df86a88e44d52ffaf8acd0ab9f431dc1d
Andreu Santaren Llop <asantaren@newrelic.com>
2018-09-12 16:09:31 +0200

    Implement basic Unit Test.

--- NewRelicVideoTests/NewRelicVideoTests.m
+++ NewRelicVideoTests/NewRelicVideoTests.m
@@ -8,6 +8,11 @@
 
 #import <XCTest/XCTest.h>
 
+#import "TrackerAutomat.h"
+#import "BackendActions.h"
+
+@import NewRelicVideo;
+
 @interface NewRelicVideoTests : XCTestCase
 
 @end
@@ -24,16 +29,97 @@
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+- (void)testBackend {
+    BackendActions *actions = [[BackendActions alloc] init];
+    [actions sendRequest];
+    [actions sendStart];
+    [actions sendEnd];
+    [actions sendPause];
+    [actions sendResume];
+    [actions sendSeekStart];
+    [actions sendSeekEnd];
+    [actions sendBufferStart];
+    [actions sendBufferEnd];
+    [actions sendHeartbeat];
+    [actions sendRenditionChange];
+    [actions sendError];
+    
+    [actions sendAdRequest];
+    [actions sendAdStart];
+    [actions sendAdEnd];
+    [actions sendAdPause];
+    [actions sendAdResume];
+    [actions sendAdSeekStart];
+    [actions sendAdSeekEnd];
+    [actions sendAdBufferStart];
+    [actions sendAdBufferEnd];
+    [actions sendAdHeartbeat];
+    [actions sendAdRenditionChange];
+    [actions sendAdError];
+    [actions sendAdBreakStart];
+    [actions sendAdBreakEnd];
+    [actions sendAdQuartile];
+    [actions sendAdClick];
+    
+    [actions sendPlayerReady];
+    [actions sendDownload];
+    
+    [actions sendAction:@"TEST_ACTION"];
+    [actions sendAction:@"TEST_ACTION" attr:@{@"testAttr": @"testValue"}];
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void)testTrackerAutomator {
+    TrackerAutomat *automat = [[TrackerAutomat alloc] init];
+    XCTAssert(automat.state == TrackerStateStopped, @"State not Stopped");
+    
+    [automat transition:TrackerTransitionClickPlay];
+    XCTAssert(automat.state == TrackerStateStarting, @"State not Starting");
+    
+    [automat transition:TrackerTransitionFrameShown];
+    XCTAssert(automat.state == TrackerStatePlaying, @"State not Playing");
+    
+    [automat transition:TrackerTransitionClickPause];
+    XCTAssert(automat.state == TrackerStatePaused, @"State not Paused");
+    
+    [automat transition:TrackerTransitionClickPlay];
+    XCTAssert(automat.state == TrackerStatePlaying, @"State not Paying");
+    
+    [automat transition:TrackerTransitionInitBuffering];
+    XCTAssert(automat.state == TrackerStateBuffering, @"State not Buffering");
+    
+    [automat transition:TrackerTransitionClickPause];
+    XCTAssert(automat.state == TrackerStateBuffering, @"State not Buffering");
+    
+    [automat transition:TrackerTransitionEndBuffering];
+    XCTAssert(automat.state == TrackerStatePlaying, @"State not Playing");
+    
+    [automat transition:TrackerTransitionInitDraggingSlider];
+    XCTAssert(automat.state == TrackerStateSeeking, @"State not Seeking");
+    
+    [automat transition:TrackerTransitionClickPlay];
+    XCTAssert(automat.state == TrackerStateSeeking, @"State not Seeking");
+    
+    [automat transition:TrackerTransitionEndDraggingSlider];
+    XCTAssert(automat.state == TrackerStatePlaying, @"State not Playing");
+    
+    [automat transition:TrackerTransitionHeartbeat];
+    XCTAssert(automat.state == TrackerStatePlaying, @"State not Playing");
+    
+    [automat transition:TrackerTransitionErrorPlaying];
+    XCTAssert(automat.state == TrackerStatePlaying, @"State not Playing");
+    
+    [automat transition:TrackerTransitionRenditionChanged];
+    XCTAssert(automat.state == TrackerStatePlaying, @"State not Playing");
+    
+    [automat transition:TrackerTransitionVideoFinished];
+    XCTAssert(automat.state == TrackerStateStopped, @"State not Stopped");
 }
+
+//- (void)testPerformanceExample {
+//    // This is an example of a performance test case.
+//    [self measureBlock:^{
+//        // Put the code you want to measure the time of here.
+//    }];
+//}
 
 @end
