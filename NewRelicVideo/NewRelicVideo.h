d921ecaa4e77e75bd0ddd3dfb83724ff264399ac
Andreu Santaren Llop <asantaren@newrelic.com>
2018-09-13 17:25:11 +0200

    Prepare umbrella header comments.

--- NewRelicVideo/NewRelicVideo.h
+++ NewRelicVideo/NewRelicVideo.h
@@ -1,24 +1,14 @@
 //
-//  NewRelicVideo.h
-//  NewRelicVideo
+//  New Relic Video Agent for Mobile -- iOS edition
 //
-//  Created by Andreu Santaren on 22/08/2018.
 //  Copyright © 2018 New Relic Inc. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
-#import <NewRelicVideo/NewRelicVideoAgent.h>
 
+#import <NewRelicVideo/NewRelicVideoAgent.h>
 //#import <NewRelicVideo/EventDefs.h>
 //#import <NewRelicVideo/Tracker.h>
 //#import <NewRelicVideo/ContentsTracker.h>
 //#import <NewRelicVideo/AdsTracker.h>
 //#import <NewRelicVideo/AVPlayerTracker.h>
-
-//! Project version number for NewRelicVideo.
-FOUNDATION_EXPORT double NewRelicVideoVersionNumber;
-
-//! Project version string for NewRelicVideo.
-FOUNDATION_EXPORT const unsigned char NewRelicVideoVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <NewRelicVideo/PublicHeader.h>
