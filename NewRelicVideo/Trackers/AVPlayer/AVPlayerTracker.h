a0afa1214c9f8e6ce9b9ec8120742764e7ccdcc4
Andreu Santaren Llop <asantaren@newrelic.com>
2018-09-14 12:55:47 +0200

    Full document AVPlayerTracker class.

--- NewRelicVideo/Trackers/AVPlayer/AVPlayerTracker.h
+++ NewRelicVideo/Trackers/AVPlayer/AVPlayerTracker.h
@@ -14,12 +14,27 @@
 /**
  `AVPlayerTracker` is the base class to manage the content events of an AVPlayer. It can be used directly or subclassed.
  */
-
 @interface AVPlayerTracker : ContentsTracker <ContentsTrackerProtocol>
 
+/**
+ Create a `AVPlayerTracker` instance using a `AVPlayer` instance.
+ 
+ @param player The `AVPlayer` object.
+ */
 - (instancetype)initWithAVPlayer:(AVPlayer *)player;
+
+/**
+ Create a `AVPlayerTracker` instance using a `AVPlayerViewController` instance.
+ 
+ @param playerViewController The `AVPlayerViewController` object.
+ */
 - (instancetype)initWithAVPlayerViewController:(AVPlayerViewController *)playerViewController;
 
+/**
+ Set the isAutoplayed state, since AVPlayer doesn't offer this property.
+ 
+ @param state A boolean `NSNumber` to indicate whether it is autpolayed or not.
+ */
 - (void)setIsAutoplayed:(NSNumber *)state;
 
 @end
