d99b77d29fc72e3e2589de26020ce39006ee0dc0
Andreu Santaren Llop <asantaren@newrelic.com>
2018-12-07 14:47:55 +0100

    Make a more complete test.

--- NewRelicVideoTests/NewRelicVideoTests.m
+++ NewRelicVideoTests/NewRelicVideoTests.m
@@ -89,36 +89,99 @@
 #pragma mark - Utils
 
 - (void)runTrackerTest:(id<TrackerProtocol>)tracker {
-    [tracker setOptionKey:@"option" value:@123];
-    [tracker setOptionKey:@"option" value:@123 forAction:@"TEST_ACTION"];
-    [tracker setTimestamp:1000 attributeName:@"timeSinceRequested"];
-    [tracker setTimestamp:1000 attributeName:@"xxxx"];
+    
+    XCTAssert([tracker isKindOfClass:AdsTracker.class] || [tracker isKindOfClass:ContentsTracker.class], @"Invalid tracker class");
     
     [tracker reset];
     [tracker setup];
+    
+    XCTAssert(tracker.state == TrackerStateStopped, @"State not Stopped");
+    
+    [tracker setOptionKey:@"option" value:@123];
+    [tracker setOptionKey:@"option" value:@123 forAction:@"TEST_ACTION"];
+    
+    XCTAssert([tracker setTimestamp:1000 attributeName:@"xxxxx"] == NO, @"Error setting unknown timestamp");
+    XCTAssert([tracker setTimestamp:1000 attributeName:@"timeSinceTrackerReady"], @"Error setting timeSinceTrackerReady");
+    XCTAssert([tracker setTimestamp:1000 attributeName:@"timeSinceLastRenditionChange"], @"Error setting timeSinceLastRenditionChange");
+    XCTAssert([tracker setTimestamp:1000 attributeName:@"timeSinceRequested"], @"Error setting timeSinceRequested");
+    
+    if ([tracker isKindOfClass:AdsTracker.class]) {
+        XCTAssert([tracker setTimestamp:1000 attributeName:@"timeSinceLastAdHeartbeat"], @"Error setting timeSinceLastAdHeartbeat");
+        XCTAssert([tracker setTimestamp:1000 attributeName:@"timeSinceAdStarted"], @"Error setting timeSinceAdStarted");
+        XCTAssert([tracker setTimestamp:1000 attributeName:@"timeSinceAdPaused"], @"Error setting timeSinceAdPaused");
+        XCTAssert([tracker setTimestamp:1000 attributeName:@"timeSinceAdBufferBegin"], @"Error setting timeSinceAdBufferBegin");
+        XCTAssert([tracker setTimestamp:1000 attributeName:@"timeSinceAdSeekBegin"], @"Error setting timeSinceAdSeekBegin");
+        XCTAssert([tracker setTimestamp:1000 attributeName:@"timeSinceAdBreakBegin"], @"Error setting timeSinceAdBreakBegin");
+        XCTAssert([tracker setTimestamp:1000 attributeName:@"timeSinceLastAdQuartile"], @"Error setting timeSinceLastAdQuartile");
+    }
+    else {
+        XCTAssert([tracker setTimestamp:1000 attributeName:@"timeSinceStarted"], @"Error setting timeSinceStarted");
+        XCTAssert([tracker setTimestamp:1000 attributeName:@"timeSincePaused"], @"Error setting timeSincePaused");
+        XCTAssert([tracker setTimestamp:1000 attributeName:@"timeSinceBufferBegin"], @"Error setting timeSinceBufferBegin");
+        XCTAssert([tracker setTimestamp:1000 attributeName:@"timeSinceSeekBegin"], @"Error setting timeSinceSeekBegin");
+        XCTAssert([tracker setTimestamp:1000 attributeName:@"timeSinceLastAd"], @"Error setting timeSinceLastAd");
+        XCTAssert([tracker setTimestamp:1000 attributeName:@"timeSinceLastHeartbeat"], @"Error setting timeSinceLastHeartbeat");
+    }
+    
+    XCTAssert(tracker.state == TrackerStateStopped, @"State not Stopped");
+    
     [tracker sendRequest];
+    XCTAssert(tracker.state == TrackerStateStarting, @"State not Starting");
+    
     [tracker sendStart];
-    [tracker sendEnd];
+    XCTAssert(tracker.state == TrackerStatePlaying, @"State not Playing");
+    
     [tracker sendPause];
+    XCTAssert(tracker.state == TrackerStatePaused, @"State not Paused");
+    
     [tracker sendResume];
-    [tracker sendSeekStart];
-    [tracker sendSeekEnd];
+    XCTAssert(tracker.state == TrackerStatePlaying, @"State not Paying");
+    
     [tracker sendBufferStart];
+    XCTAssert(tracker.state == TrackerStateBuffering, @"State not Buffering");
+    
+    [tracker sendPause];
+    XCTAssert(tracker.state == TrackerStateBuffering, @"State not Buffering");
+    
     [tracker sendBufferEnd];
+    XCTAssert(tracker.state == TrackerStatePlaying, @"State not Playing");
+    
+    [tracker sendSeekStart];
+    XCTAssert(tracker.state == TrackerStateSeeking, @"State not Seeking");
+    
+    [tracker sendSeekEnd];
+    XCTAssert(tracker.state == TrackerStatePlaying, @"State not Playing");
+    
     [tracker sendHeartbeat];
-    [tracker sendRenditionChange];
+    XCTAssert(tracker.state == TrackerStatePlaying, @"State not Playing");
+    
     [tracker sendError:@"Test error message"];
+    XCTAssert(tracker.state == TrackerStatePlaying, @"State not Playing");
+    
+    [tracker sendRenditionChange];
+    XCTAssert(tracker.state == TrackerStatePlaying, @"State not Playing");
+    
     [tracker sendPlayerReady];
     [tracker sendDownload];
+    [tracker sendHeartbeat];
     [tracker sendCustomAction:@"TEST_ACTION"];
     [tracker sendCustomAction:@"TEST_ACTION" attr:@{@"testAttr": @"testValue"}];
     
+    XCTAssert(tracker.state == TrackerStatePlaying, @"State not Playing");
+    
     if ([tracker isKindOfClass:AdsTracker.class]) {
         [(AdsTracker *)tracker sendAdBreakStart];
+        XCTAssert(tracker.state == TrackerStatePlaying, @"State not Playing");
         [(AdsTracker *)tracker sendAdClick];
+        XCTAssert(tracker.state == TrackerStatePlaying, @"State not Playing");
         [(AdsTracker *)tracker sendAdQuartile];
+        XCTAssert(tracker.state == TrackerStatePlaying, @"State not Playing");
         [(AdsTracker *)tracker sendAdBreakEnd];
+        XCTAssert(tracker.state == TrackerStatePlaying, @"State not Playing");
     }
+    
+    [tracker sendEnd];
+    XCTAssert(tracker.state == TrackerStateStopped, @"State not Stopped");
 }
 
 @end
