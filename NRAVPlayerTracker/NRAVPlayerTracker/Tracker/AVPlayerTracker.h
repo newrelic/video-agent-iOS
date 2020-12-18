6226c78f9241cf6298ac4418fa5fc47c1bc7a493
Andreu Santaren Llop <andreu.santaren@gmail.com>
2020-12-18 14:21:10 +0100

    Add NRAVPlayerTracker.

---
+++ NRAVPlayerTracker/NRAVPlayerTracker/Tracker/AVPlayerTracker.h
@@ -0,0 +1,27 @@
+//
+//  AVPlayerTracker.h
+//  NewRelicVideo
+//
+//  Created by Andreu Santaren on 05/08/2020.
+//  Copyright © 2020 New Relic Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "NRVideoTracker.h"
+
+@class AVPlayer;
+@class AVPlayerViewController;
+
+/**
+ `AVPlayerTracker` is the base class to manage the content events of an AVPlayer. It can be used directly or subclassed.
+ */
+@interface AVPlayerTracker : NRVideoTracker
+
+/**
+ Create a `AVPlayerTracker` instance using a `AVPlayer` instance.
+ 
+ @param player The `AVPlayer` object.
+ */
+- (instancetype)initWithAVPlayer:(AVPlayer *)player;
+
+@end
