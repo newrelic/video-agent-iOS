dcdcf64abe7278b96180297fc14d1aeeaedbe847
Andreu Santaren Llop <andreu.santaren@gmail.com>
2021-02-02 13:39:39 +0100

    Manage PiP.

--- Examples/iOS/PiPExampleSwift/PiPExampleSwift/ViewController.swift
+++ Examples/iOS/PiPExampleSwift/PiPExampleSwift/ViewController.swift
@@ -10,10 +10,11 @@ import AVKit
 import NewRelicVideoCore
 import NRAVPlayerTracker
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVPlayerViewControllerDelegate {
     
     private var playerController: AVPlayerViewController?
     private var trackerId: NSNumber?
+    private var inPiP: Bool = false
     
     @IBAction func clickPlayBunny(sender: UIButton) {
         playVideo(videoURL: "http://docs.evostream.com/sample_content/assets/hls-bunny-rangerequest/playlist.m3u8")
@@ -26,22 +27,14 @@ class ViewController: UIViewController {
         NewRelicVideoAgent.sharedInstance().setLogging(true)
     }
 
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        if let controller = self.playerController, controller.isBeingDismissed {
-            if let tracker = NewRelicVideoAgent.sharedInstance().contentTracker(trackerId ?? -1) as? NRVideoTracker {
-                tracker.sendEnd()
-            }
-            NewRelicVideoAgent.sharedInstance().releaseTracker(trackerId ?? -1)
-        }
-    }
-
     func playVideo(videoURL: String) {
         let player = AVPlayer.init(url: URL.init(string: videoURL)!)
         self.playerController = AVPlayerViewController.init()
         self.playerController?.player = player
         self.playerController?.showsPlaybackControls = true
+        self.playerController?.allowsPictureInPicturePlayback = true
+        self.playerController?.canStartPictureInPictureAutomaticallyFromInline = true
+        self.playerController?.delegate = self
         
         trackerId = NewRelicVideoAgent.sharedInstance().start(withContentTracker: NRTrackerAVPlayer.init(avPlayer: player))
         
@@ -49,5 +42,36 @@ class ViewController: UIViewController {
             self.playerController?.player?.play()
         }
     }
+    
+    func playerViewController(_ playerViewController: AVPlayerViewController,
+                              restoreUserInterfaceForPictureInPictureStopWithCompletionHandler completionHandler: @escaping (Bool) -> Void) {
+        present(playerViewController, animated: true) {
+            completionHandler(false)
+        }
+    }
+    
+    func playerViewController(_ playerViewController: AVPlayerViewController, willEndFullScreenPresentationWithAnimationCoordinator coordinator: UIViewControllerTransitionCoordinator) {
+                    
+        print("====> WILL END PRESENTATION")
+        
+        if !inPiP {
+            print("====> CLOSE TRACKER")
+            
+            if let tracker = NewRelicVideoAgent.sharedInstance().contentTracker(trackerId ?? -1) as? NRVideoTracker {
+                tracker.sendEnd()
+            }
+            NewRelicVideoAgent.sharedInstance().releaseTracker(trackerId ?? -1)
+        }
+    }
+    
+    func playerViewControllerDidStartPictureInPicture(_ playerViewController: AVPlayerViewController) {
+        inPiP = true
+        print("====> Start PiP")
+    }
+    
+    func playerViewControllerDidStopPictureInPicture(_ playerViewController: AVPlayerViewController) {
+        inPiP = false
+        //TODO: how to detect that PiP was closed and close tracker
+        print("====> End PiP")
+    }
 }
-
