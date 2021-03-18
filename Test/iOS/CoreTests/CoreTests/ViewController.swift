7b523938a04a57eecc40adfa30e6b1707b6f7812
Andreu Santaren Llop <andreu.santaren@gmail.com>
2021-03-18 16:00:45 +0100

    Create empty test app.

---
+++ Test/iOS/CoreTests/CoreTests/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreTests
+//
+//  Created by Andreu Santaren on 18/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
