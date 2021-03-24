a4ea67ef13cf91c536e085db018fcff0bbe128cd
Andreu Santaren Llop <andreu.santaren@gmail.com>
2021-03-24 13:26:22 +0100

    Create test app for tvOS.

---
+++ Test/tvOS/CoreTest/CoreTest/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  CoreTest
+//
+//  Created by Andreu Santaren on 24/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var textView: UITextView?
+    
+    let testArray : [TestProtocol] = [Test1(), Test2(), Test3(), Test4(), Test5(), Test6(), Test7()]
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        self.textView?.insertText("Running \(self.testArray.count) tests...\n\n\n")
+        
+        DispatchQueue.global(qos: .background).async {
+            self.testArray.forEach { (test) in
+                test.doTest(self.testResult)
+            }
+        }
+    }
+    
+    func testResult(name: String, result: Bool) {
+        DispatchQueue.main.async {
+            self.textView?.insertText("\(name)\t" + (result ? "✅" : "❌") + "\n\n")
+        }
+    }
+}
+
