9769857505e08bcf5ce286f2431d346bc72dde90
Arkaza Kumari <arkazakumari@newrelic.com>
2025-12-18 17:10:01 +0530

    fix: remove tag craetion through semantic release

--- .releaserc.js
+++ .releaserc.js
@@ -27,6 +27,13 @@ module.exports = {
       },
     ],
     '@semantic-release/changelog',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
     // [
     //   '@semantic-release/github',
     //   {
@@ -36,4 +43,5 @@ module.exports = {
     //   },
     // ],
   ],
+  tagFormat: false,
 };
